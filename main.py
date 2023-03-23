da4d05e59f3baf8b952980d16004348a46556ec3
Anna Kate Zbitkovska <124578578+AnnaKate23@users.noreply.github.com>
2023-03-23 15:19:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # python3
+import time
 
 class Query:
     def __init__(self, query):
@@ -17,31 +18,28 @@ def write_responses(result):
 def process_queries(queries):
     result = []
     # Keep list of all existing (i.e. not deleted yet) contacts.
-    contacts = []
+    contacts = {}
     for cur_query in queries:
         if cur_query.type == 'add':
+            contacts[cur_query.number] = cur_query.name
             # if we already have contact with such number,
             # we should rewrite contact's name
-            for contact in contacts:
-                if contact.number == cur_query.number:
-                    contact.name = cur_query.name
-                    break
-            else: # otherwise, just add it
-                contacts.append(cur_query)
         elif cur_query.type == 'del':
-            for j in range(len(contacts)):
-                if contacts[j].number == cur_query.number:
-                    contacts.pop(j)
-                    break
+            contacts.pop(cur_query.number, None)
+            
         else:
-            response = 'not found'
-            for contact in contacts:
-                if contact.number == cur_query.number:
-                    response = contact.name
-                    break
+            response = contacts.get(cur_query.number, 'not found')
+
+           
             result.append(response)
     return result
 
 if __name__ == '__main__':
+    start_time = time.time()
+
     write_responses(process_queries(read_queries()))
+    end_time = time.time()
+    print(end_time-start_time)
+
+
 
